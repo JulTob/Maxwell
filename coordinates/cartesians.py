3713b44295e7076014d2f96aa0969d86b55bd004
Julio <JulioToboso@gmail.com>
2019-11-06 18:04:16 +0100

    Update cartesians.py

--- coordinates/cartesians.py
+++ coordinates/cartesians.py
@@ -4,3 +4,8 @@ def modulus( Ax, Ay, Az):
     "Module of a vector"
     result = math.sqrt(Ax*Ax+Ay*Ay+Az*Az);
     return result;
+
+def unitarium( Ax, Ay, Az):
+    "Unitarium of a vector"
+    r = modulus( Ax, Ay, Az)
+    return Ax/r, Ay/r, Az/r;
