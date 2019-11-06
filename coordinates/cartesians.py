b14946a932404f24da77f37c2d61c56828024f6d
Julio <JulioToboso@gmail.com>
2019-11-06 18:23:43 +0100

    Update cartesians.py

--- coordinates/cartesians.py
+++ coordinates/cartesians.py
@@ -9,3 +9,22 @@ def unitarium( Ax, Ay, Az):
     "Unitarium of a vector"
     r = modulus( Ax, Ay, Az)
     return Ax/r, Ay/r, Az/r;
+
+def dot_product(Ax, Ay, Az, Bx, By, Bz):
+    "Dot Product of two vectors"
+    result = Ax * Bx + Ay * By + Az * Bz
+    return result
+
+
+def angle_radians(Ax, Ay, Az, Bx, By, Bz):
+    "Change of basis to cilindricals"
+    cos_angle = dot_product(Ax, Ay, Az, Bx, By, Bz) / (modulus( Ax, Ay, Az) * modulus( Bx, By, Bz))
+    angle = math.acos(cos_angle)
+    return angle;
+
+def angle(Ax, Ay, Az, Bx, By, Bz):
+    "Change of basis to cilindricals"
+    cos_angle = dot_product(Ax, Ay, Az, Bx, By, Bz) / (modulus( Ax, Ay, Az) * modulus( Bx, By, Bz))
+    angle = math.degrees(math.acos(cos_angle))
+    return angle;
+
