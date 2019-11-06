37817575b511f5e87c0bdf92cbf75586b115c284
Julio <JulioToboso@gmail.com>
2019-11-06 18:00:26 +0100

    Update sferics.py

--- coordinates/sferics.py
+++ coordinates/sferics.py
@@ -4,3 +4,7 @@ import math
 def modulus( A_r, A_theta, A_phi):
     "Module of a vector"
     return A_r;
+
+def unitarium( A_r, A_theta, A_phi):
+    "Unitarium of a vector"
+    return 1, A_theta, A_phi;
