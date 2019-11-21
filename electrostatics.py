ee121d676fa1f18018dfbcf03ea0a39e2b0bd8a1
Julio <JulioToboso@gmail.com>
2019-11-21 21:13:22 +0100

    Create electrostatics.py

---
+++ electrostatics.py
@@ -0,0 +1,19 @@
+import math
+import sympy
+from sympy import *
+init_printing(use_unicode=True)
+
+import math
+
+import coordinates
+from coordinates import *
+
+Volt = symbols(u'𝖵')
+
+
+def Electric_Field_From_Voltage_X( Voltage ):
+    Ex, Ey, Ez = X.gradient(Voltage)
+    Ex = -Ex
+    Ey = -Ey
+    Ez = -Ez
+    return (Ex, Ey, Ez)
