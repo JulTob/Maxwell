7b768d15116c0ef495f21c763bf328f1f5195891
Julio <JulioToboso@gmail.com>
2020-06-20 02:18:22 +0200

    Create T2-6_Ex.py

---
+++ Cheng/T2-6_Ex.py
@@ -0,0 +1,18 @@
+
+import electrostatics
+from electrostatics import *
+
+Qx, Qy, Qz =  3, 4, 5
+
+
+print("===========")
+print(Qx, Qy, Qz)
+
+a = PointX2C(Qx, Qy, Qz)
+pprint(a)
+
+
+
+
+
+print("===========")
